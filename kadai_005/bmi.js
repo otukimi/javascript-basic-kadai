01cfae4937dd384137744a3489ba3e44117cb165
otukimi <161011962+otukimi@users.noreply.github.com>
2024-04-01 19:42:23 +0900

    kadai_005
    kadai_005再提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,18 @@
+
+//　体重　変数の宣言・値の代入
+let weight;
+
+weight = 68;
+
+// 身長　変数の宣言・値の代入
+let height;
+
+height = 1.7;
+
+// bmi 変数の宣言・値の代入
+let bmi;
+
+bmi = weight / (height * height)
+
+// コンソールへの出力
+console.log(bmi);
